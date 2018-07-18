888de9852cd1ba3c75cf3c24b83f7e96e499ae8b
Mark Thompson <2554588+MarkTechson@users.noreply.github.com>
2018-07-18 18:58:42 -0500

    Add new components

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,78 @@
 import React, { Component } from "react";
-import { Col, Container, Row, Button} from "reactstrap";
+import { Col, Container, Row, Button, Form, FormGroup, Input, Label } from "reactstrap";
+import AwardCard from "./components/AwardCard";
+import NameCard from "./components/NameCard";
 
-class App extends Component { 
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      restaurants: [
+        {
+          name: 'Maialino',
+          genre: 'Italian',
+          score: 4.4
+        },
+        {
+          name: 'Beyond Sushi',
+          genre: 'Vegan',
+          score: 4.7
+        },
+        {
+          name: 'Abyssinia',
+          genre: 'Ethiopian',
+          score: 4.5
+        },
+        {
+          name: 'La Roja de Todos',
+          genre: 'Chilean',
+          score: 4.5
+        }
+      ],
+      users: [
+        {
+          name: "Alia",
+          userId: 10457,
+          position: "Solutions Engineer"
+        },
+        {
+          name: "Cody",
+          userId: 10850,
+          position: "Senior Functional Consultant"
+        },
+        {
+          name: "Ana",
+          userId: 10222,
+          position: "Lead Solutions Engineer"
+        },
+        {
+          name: "Leon",
+          userId: "02481",
+          position: "King of All Solution Architects in the Pacific Region of Mars"
+        }
+      ],
+      awards: [
+        {
+          id: 1,
+          title: "Best Boss Award!",
+          comment: "Thanks for always looking out for us."
+        },
+        {
+          id: 2,
+          title: "Longest Commute Award!",
+          comment: "I can't believe Leslie makes it to work as often as she does."
+        },
+        {
+          id: 3,
+          title: "Most likely to nap at work!",
+          comment: "Maybe you need more coffee."
+        }
+
+      ]
+
+    }
+  }
 
   render() {
     return (
@@ -17,13 +88,30 @@ class App extends Component {
             <Button color="success">Give Kudos</Button>
           </Col>
           <Col md="12" lg="9">
-            <img alt="award" src="http://www.pngmart.com/files/3/Award-PNG-Photos.png" width="50px" />
-            <p>Badge Name</p>
-            <img alt="avatar" src="https://www.iranketab.ir/Images/user.jpg" width="100px" />
-            <h2> Heading </h2>
-            <p>Conversion stealth influencer business-to-business entrepreneur hypotheses investor customer deployment metrics learning curve direct mailing long tail mass market. Pitch iteration stock android business-to-consumer bandwidth seed round user experience paradigm shift channels equity pivot. Metrics partner network validation responsive web design first mover advantage backing research & development market mass market innovator sales infrastructure.</p>
+            {this.state.awards.map(award => <AwardCard title={award.title} comment={award.comment} />)}
           </Col>
         </Row>
+        <Row>
+          <Col md="12">
+            <Form>
+              <FormGroup>
+                <Label>Give Kudos to</Label>
+                <Input type="select">
+                  {this.state.users.map(user => <option>({user.userId}) {user.name} {user.position}</option>)}
+                </Input>
+              </FormGroup>
+              <FormGroup>
+                <Input type="text" placeholder="Kudos Title" />
+              </FormGroup>
+              <FormGroup>
+                <Input type="text" placeholder="Kudos Text" />
+              </FormGroup>
+            </Form>
+          </Col>
+        </Row>
+        {/* NEW CODE GOES BELOW*/}
+
+
       </Container>
     );
   }
---
+++ src/client/components/NameCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NameCard = props => (
+    <div>
+        <h1>Hello, my name is {props.name} and my age is {props.age}</h1>
+    </div>
+)
+
+export default NameCard;
